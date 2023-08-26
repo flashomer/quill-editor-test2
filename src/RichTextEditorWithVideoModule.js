0c78b0b7dd080cdfbeff4dd50983ca594a4af3ff
Ömer YILDIRIM <flashomer@gmail.com>
2023-08-26 11:39:31 +0300

    test2

---
+++ src/RichTextEditorWithVideoModule.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import { Helmet } from 'react-helmet';
+import 'react-quill/dist/quill.snow.css';
+
+const RichTextEditorWithVideoModule = () => {
+    const [value, setValue] = useState('');
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, false] }, { 'font': [] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{'list': 'ordered'}, {'list': 'bullet'},
+                {'indent': '-1'}, {'indent': '+1'}],
+            ['link', 'image', 'video'],
+            ['clean']
+        ]
+    }
+
+    return <>
+        <Helmet>
+            <meta http-equiv="Content-Security-Policy" content="frame-src 'self' data: *" />
+        </Helmet>
+        <ReactQuill className="reactQuill" modules={modules} theme="snow" value={value} onChange={setValue} />
+    </>;
+};
+
+export default RichTextEditorWithVideoModule;
